e1d25908e6a6ce1e68da8ed89d469c8c7bf47884
許凱翔 <QDGNO18456@gmail.com>
2022-10-28 11:04:02 +0800

    10/28更改emperors

--- database/migrations/2022_10_11_042652_create_emperors_table.php
+++ database/migrations/2022_10_11_042652_create_emperors_table.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+
 
 class CreateEmperorsTable extends Migration
 {
@@ -16,10 +17,17 @@ class CreateEmperorsTable extends Migration
         Schema::create('emperors', function (Blueprint $table) {
             $table->id()->unsigned()->nullable(false)->comment('編號');
             $table->string('emperor_name')->nullable(false)->comment('皇帝');
-            $table->integer('dynasty_id')->unsigned()->nullable(false)->comment('朝代編號(外部鍵)');
+            
+            $table->foreignId('dynasty_id')->unsigned()->nullable(false)->comment('朝代編號(外部鍵)');
+            $table->foreign('dynasty_id')
+                ->references('id')->on('dynasties')
+                ->onDelete('cascade');
+                
             $table->integer('emperor_life')->unsigned()->nullable(true)->comment('壽命');
             $table->integer('emperor_start_year')->nullable(true)->comment('登基年');
             $table->integer('emperor_end_year')->nullable(false)->comment('退位年');
+
+            
             $table->timestamps();
         });
     }
